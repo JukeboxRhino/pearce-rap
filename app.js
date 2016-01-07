8800b6e94ae13b99c998980cf7250a7cd05f192f
Austin Pearce (Laptop) <austinpearce1005@gmail.com>
2016-01-07 12:21:09 -0500

    Partial commit, reading history still does not work

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
 var net_ping = require('net-ping');
 var net = require('net');
 var dns = require('dns');
+var fs = require('fs');
+var history = [];
+var historyChanged = false;
 /**Config**/
 var host = 'google.com';
+var historyFile = 'history.json';
+var pingIntervalMs = 5000;
+var historySaveIntervalMs = 10000;
 /**********/
+function writeHistory(history){
+	console.log('writeHistory: ' + history);
+	fs.writeFile(historyFile, history, function(err){
+		if(!err){
+			console.log('Successfully wrote history file');
+		} else {
+			console.log('Error writing history file');
+		}
+	});
+}
+function readHistory(){
+	fs.readFile(historyFile, function(err, data){
+		if(!err){
+			return JSON.parse(data);
+			console.dir(data);
+		} else {
+			return false;
+			console.log('Error reading history file');
+		}
+	});
+}
+function saveHistory(){
+	console.log('saveHistory: ' + history);
+	if(historyChanged){
+		writeHistory(JSON.stringify(history));
+		historyChanged = false;
+	}
+}
 function ping(host, callback){
 	if(net.isIPv4(host)){
 		echo(host, callback);
 	} else {
 		//Resolve DNS
-		console.log('Performing DNS lookup for ' + host);
 		dns.lookup(host, 4, function(err, addr, family){
 			if(!err){
-				console.log(host + ' resolved to ' + addr);
 				host = addr;
 				echo(host, callback);
 			} else {
@@ -72,10 +104,20 @@ function parseHistory(){
 	}
 	var percentUptime = Math.round(countSuccess / countTotal * 10000) / 100 + '%';
 	var averageTime = Math.round(timeTotal / countSuccess * 100) / 100;
-	console.log(percentUptime + ' uptime with an average round-trip time of ' + averageTime + 'ms');
+	console.log('--- ' + percentUptime + ' uptime with an average round-trip time of ' + averageTime + 'ms');
 }
-var history = [];
-setInterval(ping, 10000, host, function(result){
+//On startup, try to load history
+var loadedHistory = readHistory();
+if(loadedHistory){
+	history = loadedHistory;
+	console.log('Loaded history from file');
+	console.log('History is ' + history);
+} else {
+	console.log('Error reading history file');
+}
+setInterval(ping, pingIntervalMs, host, function(result){
 	history.push(result);
+	historyChanged = true;
 	parseHistory();
 });
+setInterval(saveHistory, historySaveIntervalMs);
