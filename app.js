e06863cff9fc2b0e169ab63e2d75384214480433
Austin Pearce (Laptop) <austinpearce1005@gmail.com>
2016-01-07 09:29:09 -0500

    Turned into function for modularity

--- app.js
+++ app.js
@@ -4,37 +4,57 @@ var dns = require('dns');
 /**Config**/
 var host = 'google.com';
 /**********/
-//Check if host is IP or domain name
-if(net.isIPv4(host)){
-	echo(host);
-} else {
-	//Resolve DNS
-	console.log('Performing DNS lookup for ' + host);
-	dns.lookup(host, 4, function(err, addr, family){
-		if(!err){
-			console.log(host + ' resolved to ' + addr);
-			host = addr;
-			echo(host);
-		} else {
-			console.log('ERROR: DNS could not resolve ' + host);
-		}
-	});
-}
-function echo(host){
+function echo(host, callback){
+	if(net.isIPv4(host)){
+		echo(host);
+	} else {
+		//Resolve DNS
+		console.log('Performing DNS lookup for ' + host);
+		dns.lookup(host, 4, function(err, addr, family){
+			if(!err){
+				console.log(host + ' resolved to ' + addr);
+				host = addr;
+				echo(host);
+			} else {
+				var result = {
+					success: false,
+					error: err,
+					type: 'DNS Lookup Error'
+				};
+				callback(result);
+				return;
+			}
+		});
+	}
 	var session = ping.createSession();
 	session.on('close', function(){
 		console.log('Socket closed');
 	});
 	session.on('error', function(err){
-		console.log('ERROR');
-		console.dir(err);
+		var result = {
+			success: false,
+			error: err,
+			type: 'Session error'
+		};
+		callback(result);
 	});
 	session.pingHost(host, function(err, target, sent, rcvd){
 		if(!err){
-			console.log(target + ' responded in ' + (rcvd.getTime() - sent.getTime()) + 'ms');
+			var result = {
+				success: true,
+				time: rcvd.getTime() - sent.getTime()
+			};
+			callback(result);
 		} else {
-			console.log('ERROR');
-			console.dir(err);
+			var result = {
+				success: false;
+				error: err,
+				type: 'Unsuccessful ping'
+			}
+			callback(result);
 		}
 	});
 }
+echo(host, function(result){
+	console.dir(result);
+});
