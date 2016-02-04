96ae3234067c0e8e287e18b67d4923c8ef565805
Austin Pearce <pearce43333@mvctc.com>
2016-02-04 13:36:18 -0500

    I fixed it

--- app.js
+++ app.js
@@ -1,26 +1,141 @@
+var colors = require('colors');
 var exec = require('child_process').exec;
+var fs = require('fs');
+var historyFile = 'history.json';
 var msregex = new RegExp(/time[=<](\d+)ms/);
 //NOTE: This only works on Windows!
+var pingInterval = 5000;//In milliseconds
+var historyInterval = 7000;//In milliseconds
+var GHistory;
+/*History Structure
+{GHistory}----
+             |
+		   [Host]
+		     |
+	      {Result}
+		     |
+ts(number) - success(Boolean) - ms(number/null) - timeOut(Boolean)
+*/
 
-function ping(host, bytes, callback){
-	if(typeof bytes == 'function'){
-		callback = bytes;
-		bytes = 32;
-	} else if(typeof bytes == 'number' && bytes >= 0 && bytes <= 65500){
-		
-	} else if(!bytes){
-		var bytes = 32;
-	}
-	exec('ping ' + host + ' -n 1 -l ' + bytes, function(err, stdout, stderr){
+function out(text){
+	var d = new Date();
+	var tag = '['+d.toLocaleTimeString()+'] ';
+	console.log(tag + text);
+}
+
+function error(text){
+	out(text.red.underline);
+}
+
+function ping(host, callback){
+	var ts = new Date();//Timestamp
+	exec('ping ' + host + ' -n 1', function(err, stdout, stderr){
 		stdout = stdout.toString().split('\r\n');
 		if(stdout[2] == 'Request timed out.'){
-			callback(1);
+			callback(1, null, host, ts);
 		} else if(msregex.exec(stdout[2])){
 			var ms = msregex.exec(stdout[2])[1];
-			callback(null, ms);
+			callback(null, ms, host, ts);
 		} else {
 			//Unknown ping response
-			callback(2);
+			callback(2, null, host, ts);
 		}
 	});
 }
+
+function startMonitoring(host){
+	if(!GHistory[host]){
+		GHistory[host] = [];
+	}
+	function resultHandler(err, result, host, ts){
+		var obj = {};
+		if(!err){
+			obj.ts = ts;
+			obj.success = true;
+			obj.ms = result;
+			obj.timeout = false;
+		} else {
+			obj.ts = ts;
+			obj.success = false;
+			if(err == 1){
+				obj.timeout = true;
+			} else {
+				obj.timeout = false;
+			}
+		}
+		GHistory[host].push(obj);
+	}
+	ping(host, resultHandler);
+	setInterval(ping, pingInterval, host, resultHandler);
+}
+
+function parseHistory(){
+	var hosts = Object.keys(GHistory);
+	for(i = 0; i < hosts.length; i++){
+		var host = hosts[i];
+		var total = GHistory[host].length;
+		var success = 0;
+		var totalms = 0;
+		var timeout = 0;
+		var unknown = 0;
+		for(j = 0; j < total; j++){
+			if(GHistory[host][j].success){
+				success++;
+				totalms += GHistory[host][j].ms;
+			} else {
+				GHistory[host][j].timeout ? timeout++ : unknown++;
+			}
+		}
+		out(host+' has '+Math.round(success/total*10000)/100+'% uptime ('+success+'/'+total+') ('+timeout+' request(s) timed out, '+unknown+' unknown error(s))');
+	}
+}
+
+function saveHistory(history, callback){
+	var data = JSON.stringify(history);
+	fs.writeFile(__dirname + '/' + historyFile, data, function(err){
+		if(!err){
+			out('Saved history');
+			if(callback){
+				callback(null);
+			}
+		} else {
+			error('Error saving history');
+			if(callback){
+				callback(err);
+			}
+		}
+	});
+}
+
+function readHistory(callback){
+	fs.readFile(__dirname + '/' + historyFile, function(err, data){
+		if(!err){
+			callback(JSON.parse(data));
+			out('Successfully read history file');
+		} else {
+			error('Error reading history file, writing new history file');
+			saveHistory({});//Save empty object
+			callback({});//Return empty object
+		}
+	});
+}
+
+process.on('SIGINT', function(){
+	out('Gracefully exiting...');
+	saveHistory(GHistory, function(err){
+		process.exit();
+	});
+});
+
+readHistory(function(history){
+	GHistory = history;
+	if(process.argv.length < 3){
+		error('Please enter one or more hosts as arguments');
+		process.exit();
+	} else {
+		for(i = 2; i < process.argv.length; i++){
+			startMonitoring(process.argv[i]);
+		}
+	}
+	setInterval(parseHistory, historyInterval);
+});
