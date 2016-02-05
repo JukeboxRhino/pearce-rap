b9f6da1a0de3ef0d71925789c83ae24910e43a0a
Austin Pearce (Laptop) <austinpearce1005@gmail.com>
2016-02-05 10:29:24 -0500

    Add batch file for CTC subdomains

---
+++ ctcdomains.bat
@@ -0,0 +1,2 @@
+node.exe app.js ae.mvctc.com cta.mvctc.com dev.mvctc.com forms.mvctc.com learnmate.mvctc.com mail.mvctc.com mobile.mvctc.com my.mvctc.com owa.mvctc.com owa1.mvctc.com portal.mvctc.com remote.mvctc.com safari.mvctc.com smtp.mvctc.com spam.mvctc.com support.mvctc.com test.mvctc.com www.mvctc.com yctube.mvctc.com
+PAUSE
